22811f3dcbf221f45bba38fcbb350e4c9cd6e278
SLMCreatives <sulaiman@slmcreatives.com>
2024-01-02 17:48:47 +0800

    First Demo

---
+++ main.js
@@ -0,0 +1,21 @@
+let toggleBtn = document.querySelector('.toggle');
+let navBar = document.querySelector('.navbar');
+let object = document.querySelector('.content');
+let overlay = document.querySelector('.overlay');
+
+
+function toggleMenu() {
+    navBar.classList.toggle('active');
+
+    object.classList.toggle('content-active');
+
+    overlay.classList.toggle('overlay-active');
+
+    if (toggleBtn.classList.contains('fa-ellipsis-v')) {
+    toggleBtn.classList.replace('fa-ellipsis-v','fa-ellipsis-h');
+} else if(toggleBtn.classList.contains('fa-ellipsis-h')) {
+    toggleBtn.classList.replace('fa-ellipsis-h','fa-ellipsis-v');
+
+}
+
+}
